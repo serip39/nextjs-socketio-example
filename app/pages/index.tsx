c69e7fa240d39611ec1eb9a82aad7599c32bf2b5
Serip39 <serip39.yuki@gmail.com>
2020-08-28 18:32:58 +0900

    feat:add socketio

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,65 +1,36 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { useState, useEffect } from 'react'
+import io from 'socket.io-client'
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+type ContainerProps = {}
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+const Container = (props: ContainerProps) => {
+  const [socket, setSocket] = useState(null)
+  const [data, setData] = useState('')
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+  useEffect(() => {
+    const client = io()
+    client.on('connect', () => {
+      console.log('socket connected!!!')
+    })
+    client.on('update-data', data => {
+      console.log('Get Updated Data', data)
+      setData(data)
+    })
+    setSocket(client)
+  }, [])
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+  return <Component {...props} data={data} />
 }
+
+type Props = {
+  className?: string
+  data: any
+} & ContainerProps
+
+const Component = (props: Props) => (
+  <div>
+    {JSON.stringify(props.data)}
+  </div>
+)
+
+export default Container
--- app/server/index.ts
+++ app/server/index.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express'
+import bodyParser from 'body-parser'
 import next from 'next'
+import socketio from "socket.io"
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
@@ -11,14 +13,32 @@ app
   .then(() => {
     const server = express()
 
+    server.use(bodyParser())
+
+    server.post('/mmwave', (req: Request, res: Response) => {
+      console.log('body', req.body)
+      postIO(req.body)
+      res.status(200).json({ message: 'success' })
+    })
+
     server.all('*', async (req: Request, res: Response) => {
       return handle(req, res)
     })
 
-    server.listen(port, (err?: any) => {
+    const httpServer = server.listen(port, (err?: any) => {
       if (err) throw err
       console.log(`> Ready on localhost:${port} - env ${process.env.NODE_ENV}`)
     })
+
+    const io = socketio.listen(httpServer)
+
+    io.on('connection', (socket: socketio.Socket) => {
+      console.log('id: ' + socket.id + ' is connected')
+    })
+
+    const postIO = (data) => {
+      io.emit('update-data', data)
+    }
   })
   .catch((ex) => {
     console.error(ex.stack)
