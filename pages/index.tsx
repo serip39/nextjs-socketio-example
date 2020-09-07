7084102e6e59bfcb40814fb7bfcceb8c2c7c9daa
Serip39 <serip39.yuki@gmail.com>
2020-09-08 01:00:43 +0900

    fix:delete unnecessary method

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import io from 'socket.io-client'
 import dayjs from 'dayjs'
